43daf392db4121f96745d2bc1ae8f07568fc7977
kalebkohn <kjkohn@wisc.edu>
2023-12-07 16:25:08 -0600

    history improvements

--- app/src/main/java/com/cs407/barhop/History.java
+++ app/src/main/java/com/cs407/barhop/History.java
@@ -18,6 +18,7 @@ import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -67,7 +68,6 @@ public class History extends AppCompatActivity {
             @Override
             public void onLocationChanged(@NonNull Location location) {
                 updateLocationInfo(location);
-                populateAndRetrieveData();
             }
         };
 
@@ -88,7 +88,6 @@ public class History extends AppCompatActivity {
                 Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                 if(location != null) {
                     updateLocationInfo(location);
-                    populateAndRetrieveData();
                 }
             }
         }
@@ -113,7 +112,12 @@ public class History extends AppCompatActivity {
                 updateHistoryInformation(bar);
             }
         }
-        populateAndRetrieveData();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                populateAndRetrieveData();
+            }
+        }, 500); // You can adjust the delay time as needed
     }
 
     @SuppressLint("StaticFieldLeak")
@@ -139,7 +143,13 @@ public class History extends AppCompatActivity {
 
             @Override
             protected void onPostExecute(Void aVoid) {
-                populateAndRetrieveData();
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        populateAndRetrieveData();
+                    }
+                }, 500); // You can adjust the delay time as neededpopulateAndRetrieveData();
             }
         }.execute();
     }
@@ -228,6 +238,7 @@ public class History extends AppCompatActivity {
                 } else {
                     TextView emptyMsg = new TextView(getBaseContext());
                     emptyMsg.setText("You have no history to show!");
+                    emptyMsg.setTextSize(50);
                     ll.addView(emptyMsg);
                 }
                 setContentView(v);
@@ -256,7 +267,7 @@ public class History extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                for(int i = 0; i < ll.getChildCount(); i++) {
+                for(int i = 0; i < ll.getChildCount() - 1; i++) {
                     LinearLayout currLayout = (LinearLayout) ll.getChildAt(i);
                     TextView text = (TextView) currLayout.getChildAt(0);
                     if (!(text.getText().toString().toLowerCase().contains(newText.toLowerCase()))) {
