f8a6a267d4c54a38ba80a90f3e37fc37dd3dd1d9
ekarbusicky <emmakarbusicky@gmail.com>
2023-12-08 19:33:30 -0600

    fixed bug in bringing up the photo selector

--- app/src/main/java/com/cs407/barhop/Profile.java
+++ app/src/main/java/com/cs407/barhop/Profile.java
@@ -5,16 +5,20 @@ import androidx.activity.result.PickVisualMediaRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
 //    ImageView xIcon;
     TextView username;
+
+    Button button;
     private UsersDao usersDao;
 
     private String currUser;
@@ -23,26 +27,13 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        button = (Button) findViewById(R.id.changePhoto);
+
         username = findViewById(R.id.username);
 
         currUser = getIntent().getStringExtra("username");
 
         username.setText(currUser);
-
-
-        // Saved here just incase we need it back
-//        xIcon = findViewById(R.id.imageViewX);
-//        xIcon.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(Profile.this, MainActivity.class);
-//                intent.putExtra("username", currUser);
-//                startActivity(intent);
-//            }
-//        });
-    }
-
-    public void changePhoto() {
 
         // Registers a photo picker activity launcher in single-select mode.
         ActivityResultLauncher<PickVisualMediaRequest> pickMedia =
@@ -56,30 +47,48 @@ public class Profile extends AppCompatActivity {
                     }
                 });
 
-        // Include only one of the following calls to launch(), depending on the types
-        // of media that you want to let the user choose from.
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("BUTTONS", "User tapped the Supabutton");
 
-        // Launch the photo picker and let the user choose images and videos.
-        pickMedia.launch(new PickVisualMediaRequest.Builder()
-                .setMediaType(ActivityResultContracts.PickVisualMedia.ImageAndVideo.INSTANCE)
-                .build());
+                // Include only one of the following calls to launch(), depending on the types
+                // of media that you want to let the user choose from.
 
-        // Launch the photo picker and let the user choose only images.
-        pickMedia.launch(new PickVisualMediaRequest.Builder()
-                .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
-                .build());
+                // Launch the photo picker and let the user choose images and videos.
+                pickMedia.launch(new PickVisualMediaRequest.Builder()
+                        .setMediaType(ActivityResultContracts.PickVisualMedia.ImageAndVideo.INSTANCE)
+                        .build());
 
-        // Launch the photo picker and let the user choose only videos.
-        pickMedia.launch(new PickVisualMediaRequest.Builder()
-                .setMediaType(ActivityResultContracts.PickVisualMedia.VideoOnly.INSTANCE)
-                .build());
+                // Launch the photo picker and let the user choose only images.
+                pickMedia.launch(new PickVisualMediaRequest.Builder()
+                        .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
+                        .build());
 
-        // Launch the photo picker and let the user choose only images/videos of a
-        // specific MIME type, such as GIFs.
-        String mimeType = "image/gif";
-        pickMedia.launch(new PickVisualMediaRequest.Builder()
-                .setMediaType(new ActivityResultContracts.PickVisualMedia.SingleMimeType(mimeType))
-                .build());
+                // Launch the photo picker and let the user choose only videos.
+                pickMedia.launch(new PickVisualMediaRequest.Builder()
+                        .setMediaType(ActivityResultContracts.PickVisualMedia.VideoOnly.INSTANCE)
+                        .build());
 
+                // Launch the photo picker and let the user choose only images/videos of a
+                // specific MIME type, such as GIFs.
+                String mimeType = "image/gif";
+                pickMedia.launch(new PickVisualMediaRequest.Builder()
+                        .setMediaType(new ActivityResultContracts.PickVisualMedia.SingleMimeType(mimeType))
+                        .build());
+
+            }
+        });
+
+        // Saved here just incase we need it back
+//        xIcon = findViewById(R.id.imageViewX);
+//        xIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Profile.this, MainActivity.class);
+//                intent.putExtra("username", currUser);
+//                startActivity(intent);
+//            }
+//        });
     }
+
 }
