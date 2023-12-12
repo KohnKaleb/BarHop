33a75d4da08b90e2a3b0138cc4452de4ac598c6c
Gabe Sepulveda <gabrielsepulveda0724@gmail.com>
2023-12-12 00:37:12 -0600

    Add Friends only shows non friends

--- app/src/main/java/com/cs407/barhop/AddFriends.java
+++ app/src/main/java/com/cs407/barhop/AddFriends.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AddFriends extends AppCompatActivity {
@@ -51,38 +53,47 @@ public class AddFriends extends AppCompatActivity {
             protected void onPostExecute(Void aVoid) {
                 // Handle any UI updates or further processing here
                 LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                View v = inflater.inflate(R.layout.activity_main, null);
-                ll = v.findViewById(R.id.parentLayout);
+                View v = inflater.inflate(R.layout.activity_add_friends, null);
+                ll = v.findViewById(R.id.addFriendsParent);
                 Users currUser = usersDao.getUser(username);
                 List<Users> allUsers = usersDao.getAllEntities();
+                List<UsersFriends> usersFriends = usersFriendsDao.getUsersFriends(currUser.getId());
+
+                List<Integer> friendIds = new ArrayList<Integer>();
+                for(UsersFriends friend: usersFriends){
+                    friendIds.add(friend.getFriendId());
+                }
 
                 int count = 0;
                 for(Users user: allUsers){
-                    LinearLayout barLayout = new LinearLayout(getBaseContext());
-                    barLayout.setOrientation(LinearLayout.VERTICAL);
-                    LinearLayout horizontal = new LinearLayout(getBaseContext());
-                    horizontal.setOrientation(LinearLayout.HORIZONTAL);
-                    TextView name = new TextView(getBaseContext());
-                    name.setText(user.getUserName());
-                    name.setTextSize(34);
-                    name.setId(count);
-                    horizontal.addView(name);
-                    Button add = new Button(getBaseContext());
-                    add.setText("Add Friend");
-                    add.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            addFriend(v, currUser, user);
-                        }
-                    });
-                    horizontal.addView(add);
-                    barLayout.addView(horizontal);
-                    Space space = new Space(getBaseContext());
-                    space.setMinimumHeight(20);
-                    barLayout.addView(space);
-                    barLayout.setId(count);
-                    count++;
-                    ll.addView(barLayout);
+                    if(user.getId() != currUser.getId() && !friendIds.contains(user.getId())) {
+                        Log.e("users", "user: " + user.getUserName());
+                        LinearLayout barLayout = new LinearLayout(getBaseContext());
+                        barLayout.setOrientation(LinearLayout.VERTICAL);
+                        LinearLayout horizontal = new LinearLayout(getBaseContext());
+                        horizontal.setOrientation(LinearLayout.HORIZONTAL);
+                        TextView name = new TextView(getBaseContext());
+                        name.setText(user.getUserName());
+                        name.setTextSize(34);
+                        name.setId(count);
+                        horizontal.addView(name);
+                        Button add = new Button(getBaseContext());
+                        add.setText("Add Friend");
+                        add.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                addFriend(v, currUser, user);
+                            }
+                        });
+                        horizontal.addView(add);
+                        barLayout.addView(horizontal);
+                        Space space = new Space(getBaseContext());
+                        space.setMinimumHeight(20);
+                        barLayout.addView(space);
+                        barLayout.setId(count);
+                        count++;
+                        ll.addView(barLayout);
+                    }
                 }
                 setContentView(v);
             }
@@ -91,5 +102,7 @@ public class AddFriends extends AppCompatActivity {
     private void addFriend(View view, Users currUser,Users user) {
         UsersFriends friend = new UsersFriends(currUser.getId(), user.getId());
         usersFriendsDao.insert(friend);
+        UsersFriends friend2 = new UsersFriends(user.getId(), currUser.getId());
+        usersFriendsDao.insert(friend2);
     }
 }
